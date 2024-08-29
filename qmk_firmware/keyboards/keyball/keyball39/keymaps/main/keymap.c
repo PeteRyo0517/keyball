59a43ce5b43b94f11a428a15ce1f588b4df97e13
PeteRyo0517 <>
2024-08-30 01:35:35 +0900

    fix: keymap add scroll snap

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/main/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/main/keymap.c
@@ -22,7 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  // keymap for default
   [0] = LAYOUT_universal(
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  ,
@@ -45,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [3] = LAYOUT_universal(
-    KC_ESC   , _______  , _______  , _______  , _______  ,                            _______  , _______  , _______  , _______  , KC_F12   ,
+    KC_ESC   , _______  , SSNP_VRT , SSNP_HOR , SSNP_FRE ,                            _______  , _______  , _______  , _______  , KC_F12   ,
     _______  , _______  , CPI_I100 , SCRL_DVI , AML_I50  ,                            _______  , _______  , KC_MS_BTN1  , KC_MS_BTN3 , KC_MS_BTN2 ,
     _______  , _______  , CPI_D100 , SCRL_DVD , AML_D50  ,                            _______  , _______  , KC_MS_BTN4  , _______    , KC_MS_BTN5 ,
     KBC_RST  , KBC_SAVE , AML_TO   , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , _______  , QK_BOOT
